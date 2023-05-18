37dced79153f946228547e4ffe5162a3ccf3a755
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-05-18 17:10:55 +0000

    syinc

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@
 // Created by: Alyssia Fung
 // Created on: march 2023
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-alyssia-fung/sw.js", {
+    scope: "/ICS2O-Unit-1-08-alyssia-fung/",
+  })
+}
 function myButtonClicked() {
   alert("Hello, Wordl!")
 }
